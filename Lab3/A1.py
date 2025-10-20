d159ee58e3366bfe30dcdbf208443c88600ccc1e
Alexandr Zhurko <119763460+UnikRCD@users.noreply.github.com>
2025-10-20 21:53:05 +0300

    Add files via upload

---
+++ Lab3/A1.py
@@ -0,0 +1,15 @@
+﻿x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+
+if x1 > 0 and y1 > 0 and x2 > 0 and y2 > 0:
+    print("Yes, I")
+elif x1 < 0 and y1 > 0 and x2 < 0 and y2 > 0:
+    print("Yes, II")
+elif x1 < 0 and y1 < 0 and x2 < 0 and y2 < 0:
+    print("Yes, III")
+elif x1 > 0 and y1 < 0 and x2 > 0 and y2 < 0:
+    print("Yes, IV")
+else:
+    print("No")
